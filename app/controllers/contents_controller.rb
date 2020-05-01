f5adc28945923af656fd515e701d382e1ae8ab93
gonshiban <nss.0120g@gmail.com>
2020-05-01 21:16:30 +0900

    新規登録機能+表示+テーブルのレイアウト修正

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,5 +1,6 @@
 class ContentsController < ApplicationController
   def index
+    @contents = Content.all
   end
 
   def show
@@ -8,6 +9,20 @@ class ContentsController < ApplicationController
   def edit
   end
 
+  def new
+    @content = Content.new
+  end
+
   def create
+    content = Content.new(content_params)
+    content.save!
+    redirect_to contents_url, notice: "タイトル「#{content.name}」を投稿しました"
+  end
+
+
+  private
+
+  def content_params
+    params.require(:content).permit(:name, :url, :description, :category, :private)
   end
 end
