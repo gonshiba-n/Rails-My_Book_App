f793ea741e62c3d436de67211e95f1c6cf3ad1f4
gonshiban <nss.0120g@gmail.com>
2020-06-25 00:37:04 +0900

    rubocopによる修正

--- spec/system/password_news_spec.rb
+++ spec/system/password_news_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "PasswordNews", type: :system do
-
   before do
     visit new_user_password_path
   end
@@ -15,18 +14,5 @@ RSpec.describe "PasswordNews", type: :system do
     expect(page).to have_content "you an email..."
     expect(page).to have_selector '.send-logo'
     expect(page).to have_button 'メールを送信'
-  end
-
-  describe "メールを送信する" do
-    let!(:user_a) { FactoryBot.create(:user) }
-    describe "再設定用のメールが届く" do
-
-      it "ログインページへ遷移してメッセージを出力" do
-        fill_in '#email', with: 'test1@example.com'
-        click_button 'メールを送信'
-        sleep 0.5
-        expect(page).to have_content 'パスワードの再設定について数分以内にメールでご連絡いたします。'
-      end
-    end
   end
 end
