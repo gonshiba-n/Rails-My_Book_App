2362ed6bed1e3452293a4033ddda120a54a2daea
gonshiban <nss.0120g@gmail.com>
2020-07-17 08:44:24 +0900

    view微調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_many :contents, dependent: :destroy
   has_one_attached :user_image, dependent: :destroy
 
-  has_many :relationships
-  has_many :followings, through: :relationships, source: :follow
+  has_many :relationships, dependent: :destroy
+  has_many :followings, through: :relationships, source: :follow, dependent: :destroy
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverse_of_relationships, source: :user
 
