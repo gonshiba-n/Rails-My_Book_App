65018bef56aeda4f459595d997a084af065a2bd1
gonshiban <nss.0120g@gmail.com>
2020-07-17 09:26:31 +0900

    gitignore修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   has_many :contents, dependent: :destroy
   has_one_attached :user_image, dependent: :destroy
 
-  has_many :relationships, dependent: :destroy
+  has_many :relationships
   has_many :followings, through: :relationships, source: :follow, dependent: :destroy
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :user
 
   has_many :contents
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :fav_content, through: :favorites, source: :content
 
   validate :validate_user_image
