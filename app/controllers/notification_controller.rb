22d76a83b39c1f059ac241b9a9d6bceb9ce87192
gonshiban <nss.0120g@gmail.com>
2020-07-23 04:23:15 +0900

    rails gでコントローラー作成indexのみ.

---
+++ app/controllers/notification_controller.rb
@@ -0,0 +1,10 @@
+class NotificationController < ApplicationController
+  def index
+    # 相手からの通知のみ抽出
+    @notification = current_user.passive_notifications.page(params[:page]).per(20)
+    # 通知未確認(false)から確認(true)へデータ分、繰り返し変更
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
+  end
+end
