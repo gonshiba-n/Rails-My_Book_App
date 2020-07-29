f0139bb0bc7d8a327f15208e60178c9f936b3208
gonshiban <nss.0120g@gmail.com>
2020-07-30 00:04:52 +0900

    不要なCSS,JSファイル削除

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,5 @@ class SearchController < ApplicationController
   def index
     @q = User.where.not(id: current_user.id).ransack(params[:q])
     @users = @q.result(distinct: true).page(params[:page]).per(10)
-    @contents = Content.where(private: [1, 2]).order(updated_at: :desc)
   end
 end
