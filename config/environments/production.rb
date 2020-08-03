1dce0bf06cc6c41392602e6a4ffe288d8e72ec51
gonshiban <nss.0120g@gmail.com>
2020-08-04 00:56:24 +0900

    mailer_setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'https://mybook-gn.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.smtp_settings = {
     :user_name => ENV["SENDGRID_USERNAME"],
