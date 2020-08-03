f82026e7caec28c69785352b17ec61412e5b2ec5
gonshiban <nss.0120g@gmail.com>
2020-08-03 23:04:11 +0900

    mailer_test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
-    :user_name => ENV["MAILER_NAME"],
-    :password => ENV["MAILER_PASSWORD"],
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
     :domain => "heroku.com",
     :address => "smtp.sendgrid.net",
     :port => 587,
