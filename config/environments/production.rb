42096777ec0fdb7e0889bcf0a7eae654c9bd8477
gonshiban <nss.0120g@gmail.com>
2020-08-04 01:02:35 +0900

    mailer_setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
     :address => "smtp.sendgrid.net",
     :port => 587,
     :authentication => :plain,
-    :enable_starttls_auto => true
+    :enable_starttls_auto => false
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
